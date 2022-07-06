4970bc41347bcc75ec76b9d983243928db011737
Ethan Snow <bombitmanbomb@gmail.com>
2022-07-06 07:46:14 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const port = 3000;
+import Express from "express";
+import api from "./api/api.mjs";
+const app = Express()
+app.use(Express.static("./dist")) // Enable Frontend Static Site
+
+app.use("/api",api)
+app.listen(port,()=>{console.log(`Listening on port ${port}.`)});
