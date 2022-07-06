145fc91ec64bc8a664863dcc93d0cc6ba47bf4c7
Ethan Snow <bombitmanbomb@gmail.com>
2022-07-06 07:46:55 -0400

    Create api.mjs

---
+++ api/api.mjs
@@ -0,0 +1,33 @@
+import { Router, json } from "express";
+import { ResponseFactory } from "./response/responseFactory.mjs";
+import apiRouting from "./router.mjs";
+const api = Router()
+api.use(json());
+//! Response Injection Middleware
+api.use((req, res, next) => {
+  //? Inject ResponseFactory into mutable Request object.
+  req.respond = ResponseFactory;
+  //* Continue to Routing
+  next()
+})
+
+api.use(apiRouting);
+
+//! Error Handling
+api.use((req, res, next) => {
+  let resource = req.originalUrl.substring(4);
+  resource = resource.endsWith("/")?resource:resource+"/"
+  return next(req.respond.Error(404, "Resource not Found", { resource }));
+});
+
+api.use((response, req, res, next) => {
+  if (response instanceof ResponseFactory.ApiResponse) {
+    return res.set("Content-Type", "application/json").status(response.code).json(response);
+  } else if (response instanceof Error) {
+    return res.set("Content-Type", "application/json").status(500).json(req.respond.Error(500, response));
+  } else {
+    console.log("Something went Wrong",response);
+    return res.set("Content-Type", "application/json").status(500).json(req.respond.Error(500, "Something went Wrong"));
+  }
+})
+export default api;
