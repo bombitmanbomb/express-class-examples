16b2c3a4a9a2fed4591a0f449c50da9cd2d4a49e
Ethan Snow <bombitmanbomb@gmail.com>
2022-07-06 07:46:49 -0400

    Create Database Wrapper

---
+++ database/database.mjs
@@ -0,0 +1,30 @@
+import { join, dirname } from 'path'
+import { Low, JSONFile } from 'lowdb'
+import { fileURLToPath } from 'url'
+import lodash from 'lodash'
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+await db.read();
+db.data ??= {
+  users: [],
+  books: []
+}
+await db.write()
+db.chain = lodash.chain(db.data)
+export class Database {
+  static get data(){
+    return db.data
+  }
+  static get chain(){
+    return db.chain
+  }
+  static read(){
+    return db.read()
+  }
+  static write(){
+    return db.write()
+  }
+}
