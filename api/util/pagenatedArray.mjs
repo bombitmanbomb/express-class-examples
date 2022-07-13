b703c1a0a8ca7f27d697d2e30643d2d7462d0d78
Ethan Snow <bombitmanbomb@gmail.com>
2022-07-13 09:15:29 -0400

    Create pagenatedArray.mjs

---
+++ api/util/pagenatedArray.mjs
@@ -0,0 +1,107 @@
+import _ from "lodash";
+/**
+ * @template T
+ */
+export class PagenatedArray {
+  /**
+   * @type {T[]}
+   */
+  _raw;
+  /**
+   * @type {number}
+   */
+  _page;
+  /**
+   * @type {number}
+   */
+  _size;
+  get raw() {
+    return this._raw
+  }
+  get page() {
+    return this._page
+  }
+  get size() {
+    return this._size
+  }
+
+  set raw(value) {
+    if (this._raw != value) this.markCacheDirty();
+    this._raw = value;
+  }
+  set page(value) {
+    if (this._page != value) this.markCacheDirty();
+    this._page = value;
+  }
+  set size(value) {
+    if (this._size != value) this.markCacheDirty();
+    this._size = value;
+  }
+  /**
+   *Creates an instance of PagenatedArray.
+   * @param { T[] | { items:T[], page:number, size:number } } [arr=[]]
+   * @param { number } [page=0]
+   * @param { number } [size=25]
+   * @memberof PagenatedArray
+   */
+  constructor(arr = [], page = 0, size = 25) {
+    if (Array.isArray(arr)) {
+      this.raw = arr;
+      this.page = page
+      this.size = size
+    } else {
+      this.raw = arr.items
+      this.page = arr.page
+      this.size = arr.size
+    }
+
+    //? Init
+    this._cacheDirty = true
+    this._cache = {
+      page: 0,
+      size: 0,
+      total_pages: 0,
+      total: 0,
+      items: new Array()
+    }
+  }
+
+  markCacheDirty() {
+    this._cacheDirty = true
+  }
+  createCache() {
+    if (!this._cacheDirty) return;
+    const
+      totalItems = this.raw.length,
+      pageNumber = this.page,
+      pageSize = this.size,
+      offset = (pageNumber - 1) * pageSize,
+      pagedItems = _.chain(this.raw).drop(offset).slice(0, pageSize).value();
+
+    this._cache.page = pageNumber;
+    this._cache.size = pageSize;
+    this._cache.total_pages = Math.ceil(totalItems / pageSize);
+    this._cache.total = pagedItems.length;
+    this._cache.items = pagedItems;
+    this._cacheDirty = false
+  }
+  get total_pages() {
+    this.createCache()
+    return this._cache.total_pages
+  }
+  /**
+   * @type {T[]}
+   */
+  get items() {
+    this.createCache()
+    return this._cache.items
+  }
+  get total() {
+    this.createCache()
+    return this._cache.total
+  }
+  toJSON() {
+    this.createCache()
+    return this._cache
+  }
+}
